2891b313acc1fcd68a0a522bb9064a734df1ae55
GabrielAB2411 <gabrielab2411@gmail.com>
2021-03-03 00:48:16 -0300

    Aula04

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { SendEmailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import {UserController} from "./controllers/UserController";
 
@@ -6,10 +7,12 @@ const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendMailController = new SendEmailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
+router.post("/sendMail", sendMailController.execute)
 
 export {router};
